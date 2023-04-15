eb3b786fa690069c6e25962ab659d35894ad7eec
Akezhan Rakishev <akezhan.rakishev@tvh.com>
2023-04-16 02:12:57 +0600

    feat: order tracking

---
+++ pages/order-traking.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+
+const OrderTraking: NextPage = () => {
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
+};
+
+export default OrderTraking;
